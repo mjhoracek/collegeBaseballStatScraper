d4c2bd73dea81d0b488fdc2680c8e4647b77ee5c
Mitch HoraceK <mitch@inthezone.dev>
2021-03-25 22:04:13 -0700

    Split double dip data key:values

--- jsonScraper.py
+++ jsonScraper.py
@@ -5,14 +5,24 @@ from bs4 import BeautifulSoup
 from csv import writer
 
 # Enter URL of XML Baseball Stat sheet
-url = 'https://dartmouthsports.com/sports/baseball/stats/2013'
+# url = 'https://dartmouthsports.com/sports/baseball/stats/2013'
+# url = 'https://uclabruins.com/sports/baseball/stats'
+url = 'https://fightingillini.com/sports/baseball/stats'
+
 
 # Collect HTML w/Beautiful Soup
 response = requests.get(url)
 soup = BeautifulSoup( response.text, 'html.parser' )
 
+# Find team name via title tag
+title = soup.find('title').get_text()
+title = title.replace(' ', '_').replace('\r', '').replace("\n", '').replace('\t', '')
+csvTitle = str(title)
+csvTitle = '%s.csv' % csvTitle
+
 # Isolate Tables for hitters and pitchers data
 hitters = soup.find_all('table')[0]
+
 # Remove tfoot from table, which contains unwanted totals/opponents data
 for tfoot in hitters.find_all('tfoot', recursive=True):
     tfoot.decompose()
@@ -26,7 +36,7 @@ hitterFields = []
 #Initialize table_data, where all of the data is going to live. This is an array of dictionaries
 table_data = []
 
-#Build Hitters data
+# Build Hitters data
 for tr in hitters.find_all('tr', recursive=True):
     for th in tr.find_all('th', recursive=True):
         hitterFields.append(th.get_text())
@@ -37,9 +47,8 @@ for tr in hitters.find_all('tr', recursive=True):
     if datum:
         table_data.append(datum)
 
-
+# Build Pitchers Data
 pitcherFields = []
-
 for tr in pitchers.find_all('tr', recursive=True):
     for th in tr.find_all('th', recursive=True):
         pitcherFields.append(th.get_text())
@@ -50,18 +59,72 @@ for tr in pitchers.find_all('tr', recursive=True):
     if pitcherData:
         table_data.append(pitcherData)
 
-# Removes unwanted key/value pairs at the end of each dictionary
+###### Data Cleaning Block ######
 index = 0
 while index < len(table_data):
+
+    # Remove unwanted k/v junk at the end of each dict
     table_data[index].popitem()
     table_data[index].popitem()
-    index +=1
+
+    # Add team info k/v
+    table_data[index].update({'dataSet' : title})
+
+    # Split "Player" key into first/last names, remove "Player" 
+    name = table_data[index]['Player'].split(', ')
+    table_data[index].update({'firstName' : name[0]})
+    table_data[index].update({'lastName' : name[1]})
+    table_data[index].pop('Player')
+
+    # Separate double values in hitter/pitcher data
+    gpgs = 'GP-GS'
+    if gpgs in table_data[index]:
+        games = table_data[index]['GP-GS'].split('-')
+        table_data[index].update({'gamesPlayed' : games[0]})
+        table_data[index].update({'gamesStarted' : games[1]})
+        table_data[index].pop('GP-GS')
+
+    sbatt = 'SB-ATT'
+    if sbatt in table_data[index]:
+        sb = table_data[index]['SB-ATT'].split('-')
+        table_data[index].update({'stolenBases' : sb[0]})
+        table_data[index].update({'attempts' : sb[1]})
+        table_data[index].pop('SB-ATT')
+         
+    winloss = 'W-L'
+    if winloss in table_data[index]:
+        win = table_data[index]['W-L'].split('-')
+        table_data[index].update({'wins' : win[0]})
+        table_data[index].update({'losses' : win[1]})
+        table_data[index].pop('W-L')
+
+    appgs = 'APP-GS'
+    if appgs in table_data[index]:
+        win = table_data[index]['APP-GS'].split('-')
+        table_data[index].update({'appearances' : win[0]})
+        table_data[index].update({'gamesStarted' : win[1]})
+        table_data[index].pop('APP-GS')
+
+    sho = 'SHO'
+    if sho in table_data[index]:
+        shutouts = table_data[index]['SHO'].split('-')
+        table_data[index].update({'shutouts' : shutouts[0]})
+        table_data[index].update({'completeGames' : shutouts[1]})
+        table_data[index].pop('SHO')
+
+    index += 1
+
+##### End Data Cleaning Block #####
 
 
-print(json.dumps(table_data, indent=4))
-# print(table_data[1]['AVG'])
 
-# keller = table_data[0]
-# print(json.dumps(keller, indent=4))
+# print(table_data[5]['GP-GS'].split('-'))
+
+tableLength = len(table_data)
+
+print(f'***{tableLength} players found for {title}***')
+print(json.dumps(table_data[0], indent=4))
+print(json.dumps(table_data[tableLength-8], indent=4))
+
 
 # print("mission success")
