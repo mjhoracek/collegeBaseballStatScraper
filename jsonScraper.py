d1a6bd7688faf9397d103bc06a9e6fa459528090
Mitch HoraceK <mitch@inthezone.dev>
2021-03-26 21:02:05 -0700

    Cleaned up data

--- jsonScraper.py
+++ jsonScraper.py
@@ -76,40 +76,168 @@ while index < len(table_data):
     table_data[index].update({'lastName' : name[1]})
     table_data[index].pop('Player')
 
+    #Convert stats into integers from strings
+    Num = '#'
+    if Num in table_data[index]:
+        table_data[index][Num] = int(table_data[index][Num])
+    
+    ERA = 'ERA'
+    if ERA in table_data[index]:
+        try:
+            table_data[index][ERA] = float(table_data[index][ERA])
+        except:
+            table_data[index][ERA]
+
+    WHIP = 'WHIP'
+    if WHIP in table_data[index]:
+        try:
+            table_data[index][WHIP] = float(table_data[index][WHIP])
+        except:
+            table_data[index][WHIP]
+
+    CG = 'CG'
+    if CG in table_data[index]:
+        table_data[index][CG] = int(table_data[index][CG])
+
+    HBP = 'HBP'
+    if HBP in table_data[index]:
+        table_data[index][HBP] = int(table_data[index][HBP])
+
+    SV = 'SV'
+    if SV in table_data[index]:
+        table_data[index][SV] = int(table_data[index][SV])
+    
+    IP = 'IP'
+    if IP in table_data[index]:
+        table_data[index][IP] = float(table_data[index][IP])
+    
+    H = 'H'
+    if H in table_data[index]:
+        table_data[index][H] = int(table_data[index][H])
+    
+    R = 'R'
+    if R in table_data[index]:
+        table_data[index][R] = int(table_data[index][R])
+
+    ER = 'ER'
+    if ER in table_data[index]:
+        table_data[index][ER] = int(table_data[index][ER])
+    
+    BB = 'BB'
+    if BB in table_data[index]:
+        table_data[index][BB] = int(table_data[index][BB])
+
+    SO = 'SO'
+    if SO in table_data[index]:
+        table_data[index][SO] = int(table_data[index][SO])
+
+    Double = '2B'
+    if Double in table_data[index]:
+        table_data[index][Double] = int(table_data[index][Double])
+
+    Triple = '3B'
+    if Triple in table_data[index]:
+        table_data[index][Triple] = int(table_data[index][Triple])
+
+    HR = 'HR'
+    if HR in table_data[index]:
+        table_data[index][HR] = int(table_data[index][HR])
+
+    AB = 'AB'
+    if AB in table_data[index]:
+        table_data[index][AB] = int(table_data[index][AB])
+
+    BAVG = 'B/AVG'
+    if BAVG in table_data[index]:
+        table_data[index][BAVG] = float(table_data[index][BAVG])
+
+    WP = 'WP'
+    if WP in table_data[index]:
+        table_data[index][WP] = int(table_data[index][WP])
+    
+    BK = 'BK'
+    if BK in table_data[index]:
+        table_data[index][BK] = int(table_data[index][BK])
+
+    SFA = 'SFA'
+    if SFA in table_data[index]:
+        table_data[index][SFA] = int(table_data[index][SFA])
+
+    SHA = 'SHA'
+    if SHA in table_data[index]:
+        table_data[index][SHA] = int(table_data[index][SHA])
+
+    AVG = 'AVG'
+    if AVG in table_data[index]:
+        table_data[index][AVG] = float(table_data[index][AVG])
+
+    OPS = 'OPS'
+    if OPS in table_data[index]:
+        table_data[index][OPS] = float(table_data[index][OPS])
+
+    RBI = 'RBI'
+    if RBI in table_data[index]:
+        table_data[index][RBI] = int(table_data[index][RBI])
+
+    TB = 'TB'
+    if TB in table_data[index]:
+        table_data[index][TB] = int(table_data[index][TB])
+
+    SLG = 'SLG%'
+    if SLG in table_data[index]:
+        table_data[index][SLG] = float(table_data[index][SLG])
+    
+    GDP = 'GDP'
+    if GDP in table_data[index]:
+        table_data[index][GDP] = int(table_data[index][GDP])
+
+    OB = 'OB%'
+    if OB in table_data[index]:
+        table_data[index][OB] = float(table_data[index][OB])
+
+    SF = 'SF'
+    if SF in table_data[index]:
+        table_data[index][SF] = int(table_data[index][SF])
+
+    SH = 'SH'
+    if SH in table_data[index]:
+        table_data[index][SH] = int(table_data[index][SH])
+
+    
     # Separate double values in hitter/pitcher data
     gpgs = 'GP-GS'
     if gpgs in table_data[index]:
         games = table_data[index]['GP-GS'].split('-')
-        table_data[index].update({'gamesPlayed' : games[0]})
-        table_data[index].update({'gamesStarted' : games[1]})
+        table_data[index].update({'gamesPlayed' : int(games[0])})
+        table_data[index].update({'gamesStarted' : int(games[1])})
         table_data[index].pop('GP-GS')
 
     sbatt = 'SB-ATT'
     if sbatt in table_data[index]:
         sb = table_data[index]['SB-ATT'].split('-')
-        table_data[index].update({'stolenBases' : sb[0]})
-        table_data[index].update({'attempts' : sb[1]})
+        table_data[index].update({'stolenBases' : int(sb[0])})
+        table_data[index].update({'attempts' : int(sb[1])})
         table_data[index].pop('SB-ATT')
          
     winloss = 'W-L'
     if winloss in table_data[index]:
         win = table_data[index]['W-L'].split('-')
-        table_data[index].update({'wins' : win[0]})
-        table_data[index].update({'losses' : win[1]})
+        table_data[index].update({'wins' :int( win[0])})
+        table_data[index].update({'losses' : int(win[1])})
         table_data[index].pop('W-L')
 
     appgs = 'APP-GS'
     if appgs in table_data[index]:
         win = table_data[index]['APP-GS'].split('-')
-        table_data[index].update({'appearances' : win[0]})
-        table_data[index].update({'gamesStarted' : win[1]})
+        table_data[index].update({'appearances' : int(win[0])})
+        table_data[index].update({'gamesStarted' : int(win[1])})
         table_data[index].pop('APP-GS')
 
     sho = 'SHO'
     if sho in table_data[index]:
         shutouts = table_data[index]['SHO'].split('-')
-        table_data[index].update({'completeGames' : shutouts[0]})
-        table_data[index].update({'shutouts' : shutouts[1]})
+        table_data[index].update({'completeGames' : int(shutouts[0])})
+        table_data[index].update({'shutouts' : int(shutouts[1])})
         table_data[index].pop('SHO')
 
     index += 1
